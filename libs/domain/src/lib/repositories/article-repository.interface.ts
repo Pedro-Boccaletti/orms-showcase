a8dd28b10efbf73dbc73a6c2c272b8d588d9f360
Pedro Boccaletti <boccaletti.pedro@gmail.com>
2025-07-22 15:10:02 -0300

    feat(repositories): change repository from interfaces to abstract classes with additional methods and add repository tokens

--- libs/domain/src/lib/repositories/article-repository.interface.ts
+++ libs/domain/src/lib/repositories/article-repository.interface.ts
@@ -2,10 +2,21 @@ import { CreateArticleDto } from '../dtos/create-article.dto';
 import { UpdateArticleDto } from '../dtos/update-article.dto';
 import { Article } from '../entities/article.entity';
 
-export interface IArticleRepository {
-  findAll(): Promise<Article[]>;
-  findById(id: string): Promise<Article | null>;
-  create(article: CreateArticleDto): Promise<Article>;
-  update(id: string, article: UpdateArticleDto): Promise<Article | null>;
-  delete(id: string): Promise<boolean>;
+export interface FetchArticlesOptions {
+  includeComments?: boolean;
+}
+
+export abstract class IArticleRepository {
+  abstract findAll(options?: FetchArticlesOptions): Promise<Article[]>;
+  abstract findById(
+    id: string,
+    options?: FetchArticlesOptions
+  ): Promise<Article | null>;
+  abstract create(article: CreateArticleDto): Promise<Article>;
+  abstract update(
+    id: string,
+    article: UpdateArticleDto
+  ): Promise<Article | null>;
+  abstract delete(id: string): Promise<boolean>;
+  abstract findByAuthorId(authorId: string): Promise<Article[]>;
 }
--- libs/domain/src/lib/repositories/comment-repository.interface.ts
+++ libs/domain/src/lib/repositories/comment-repository.interface.ts
@@ -2,8 +2,14 @@ import { CreateCommentDto } from '../dtos/create-comment.dto';
 import { Comment } from '../entities/comment.entity';
 import { UpdateCommentDto } from '../dtos/update-comment.dto';
 
-export interface ICommentRepository {
-  create(comment: CreateCommentDto & { articleId: string }): Promise<Comment>;
-  update(id: string, comment: UpdateCommentDto): Promise<Comment | null>;
-  delete(id: string): Promise<boolean>;
+export abstract class ICommentRepository {
+  abstract create(
+    comment: CreateCommentDto & { articleId: string }
+  ): Promise<Comment>;
+  abstract update(
+    id: string,
+    comment: UpdateCommentDto
+  ): Promise<Comment | null>;
+  abstract delete(id: string): Promise<boolean>;
+  abstract findByArticleId(articleId: string): Promise<Comment[]>;
 }
