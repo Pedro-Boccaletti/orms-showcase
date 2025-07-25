efa35c289fe09094e791eb75acc157a71586bc22
Pedro Boccaletti <boccaletti.pedro@gmail.com>
2025-07-25 15:40:26 -0300

    feat(users): add missing parameter decorators in user controller

--- libs/api/core/src/lib/users/users.controller.ts
+++ libs/api/core/src/lib/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Post } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Param, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 
@@ -12,17 +12,17 @@ export class UsersController {
   }
 
   @Get(':id')
-  findOne(id: string) {
+  findOne(@Param('id') id: string) {
     return this.usersService.findById(id);
   }
 
   @Post()
-  create(user: CreateUserDto) {
+  create(@Body() user: CreateUserDto) {
     return this.usersService.create(user);
   }
 
   @Delete(':id')
-  delete(id: string) {
+  delete(@Param('id') id: string) {
     return this.usersService.delete(id);
   }
 }
