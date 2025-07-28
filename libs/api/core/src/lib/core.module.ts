0e329ebd575e145f9760f81ed165ac6cc42f460b
Pedro Boccaletti <boccaletti.pedro@gmail.com>
2025-07-28 14:22:42 -0300

    ref(*): move repository configuration module to core, for easier testing

--- libs/api/core/src/lib/core.module.ts
+++ libs/api/core/src/lib/core.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ArticlesModule } from './articles/articles.module';
 import { UsersModule } from './users/users.module';
+import {
+  RepositoryConfigModule,
+  Repositories,
+} from './config/repository.config';
 
 @Module({
-  imports: [ArticlesModule, UsersModule],
+  imports: [
+    // Repository configuration module
+    RepositoryConfigModule.forRoot(
+      process.env['REPOSITORY_TYPE'] as Repositories
+    ),
+    ArticlesModule,
+    UsersModule,
+  ],
   providers: [],
 })
 export class CoreModule {}
